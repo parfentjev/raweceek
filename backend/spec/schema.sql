c8a91fb5bf0bfb338abac3e93ca4cba917c93f76
Aleksei Parfentjev <aleksei.parfentjev@tutamail.com>
2025-03-04 18:24:23 +0000

    add index

--- backend/spec/schema.sql
+++ backend/spec/schema.sql
@@ -5,3 +5,5 @@ CREATE TABLE `session` (
   `start_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+CREATE INDEX `idx_session_start_time` ON session(`start_time`);
