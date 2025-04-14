70a722738dac90902619add1c04aee714b454468
Aleksei Parfentjev <aleksei.parfentjev@tutamail.com>
2025-04-14 19:46:06 +0300

    change eye blinks calculation parfentjev/raweceek#2

--- backend/service/src/main/java/eu/raweceek/service/session/service/CountdownService.java
+++ backend/service/src/main/java/eu/raweceek/service/session/service/CountdownService.java
@@ -1,31 +1,33 @@
 package eu.raweceek.service.session.service;
 
-import eu.raweceek.codegen.models.CountdownDto;
-import eu.raweceek.codegen.models.SessionDto;
-import org.springframework.stereotype.Service;
-
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
+import eu.raweceek.codegen.models.CountdownDto;
+import eu.raweceek.codegen.models.SessionDto;
+
 @Service
 public class CountdownService {
-  private static final Map<CountdownDto.UnitEnum, Double> UNIT_SECONDS_MAP = Map.of(
-    CountdownDto.UnitEnum.CEEKS, 604_800.0,
-    CountdownDto.UnitEnum.SUPER_MAXES, 225.0,
-    CountdownDto.UnitEnum.DOG_YEARS, 4_505_142.0,
-    CountdownDto.UnitEnum.EYE_BLINKS, 0.3
-  );
 
-  public List<CountdownDto> calculateRemainingTime(SessionDto sessionDto) {
-    var targetTime = DateTimeFormatter.ISO_DATE_TIME.parse(sessionDto.getStartTime(), Instant::from).getEpochSecond();
-    var currentTime = Instant.now().getEpochSecond();
-    var remainingTime = targetTime - currentTime;
+    private static final Map<CountdownDto.UnitEnum, Double> UNIT_SECONDS_MAP = Map.of(
+            CountdownDto.UnitEnum.CEEKS, 604_800.0,
+            CountdownDto.UnitEnum.SUPER_MAXES, 225.0,
+            CountdownDto.UnitEnum.DOG_YEARS, 4_505_142.0,
+            CountdownDto.UnitEnum.EYE_BLINKS, 3.0
+    );
 
-    return UNIT_SECONDS_MAP.entrySet()
-      .stream()
-      .map(e -> new CountdownDto().value(remainingTime / e.getValue()).unit(e.getKey()))
-      .toList();
-  }
+    public List<CountdownDto> calculateRemainingTime(SessionDto sessionDto) {
+        var targetTime = DateTimeFormatter.ISO_DATE_TIME.parse(sessionDto.getStartTime(), Instant::from).getEpochSecond();
+        var currentTime = Instant.now().getEpochSecond();
+        var remainingTime = targetTime - currentTime;
+
+        return UNIT_SECONDS_MAP.entrySet()
+                .stream()
+                .map(e -> new CountdownDto().value(remainingTime / e.getValue()).unit(e.getKey()))
+                .toList();
+    }
 }
